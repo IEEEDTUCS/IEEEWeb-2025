9d7b7f36ec0528b3a1d8d758f88117e48afd03d0
komal-rathore-100105 <24.a09.027@gmail.com>
2025-08-25 15:55:21 +0530

    testimonials work done

--- frontend/src/components/Council/council.jsx
+++ frontend/src/components/Council/council.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function council() {
   return (
-    <div>council</div>
+    <div>
+      <h1>IEEE DTU Council</h1>
+      <p>Welcome to the council page. Here we will showcase our team members and activities.</p>
+      council</div>
   )
 }
