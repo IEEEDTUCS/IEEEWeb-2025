8c1cd9913806c94ce60d76a94cf0281f528cc40a
Zahanboi <zahansharma123@gmail.com>
2025-10-02 22:36:22 +0530

    backend updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createServer } from "node:http";
 import cors from "cors";
-import mongoose from "mongoose";
+import dbConnect from "./init/index.js";
 
 const app = express();
 const server = createServer(app);   // Explicit server
@@ -12,11 +12,7 @@ app.use(express.json({ limit: "40kb" }))
 app.use(express.urlencoded({ limit: "40kb", extended: true  }))
 
 const startServer = async () => {
-
-    // app.set("mongo_user");
-    // const connectionDb = await mongoose.connect(process.env.MONGO_URL);
-
-    // console.log(`Connected to MongoDB ${connectionDb.connection.host}`);
+    await dbConnect();  
     app.get("/", (req, res) => {
         res.json({ message: "Welcome to IEEEDTU Backend" });
     });
--- backend/src/init/index.js
+++ backend/src/init/index.js
@@ -1,1 +1,33 @@
-//initialise data and connection with database
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import {User} from '../models/users.js'
+dotenv.config();
+const dbConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`\nMONGODB connected :: DB_HOST :: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB CONNECTION FAILED ::", error);
+        process.exit(1);
+    }
+}
+
+const init = async ()=>{
+    const data = {
+        email: "test@example.com",
+        password: "password123"
+    }
+    
+    const user = await User.create({
+            email: data.email.toLowerCase(),
+            password: data.password
+        });
+
+        await user.save();
+        if(user) console.log("Sample user created");
+}
+
+dbConnect();
+init();
+
+export default dbConnect;
