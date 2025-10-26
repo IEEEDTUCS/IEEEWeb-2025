5319265cf4e2138db6213d75599a44a50a12dfa8
KaranGupta_2006 <guptakaran.port@gmail.com>
2025-10-26 12:48:46 +0530

    Nodemailer added

--- backend/src/Controllers/emailController.js
+++ backend/src/Controllers/emailController.js
@@ -1,5 +1,6 @@
 import Email from "../models/emailModel.js";
 import expressError from "../utils/errorHandler.js";
+import nodeMailer from "nodemailer";
 
 export const saveEmail = async (req, res, next) => {
   const { email } = req.body;
@@ -26,4 +27,40 @@ export const saveEmail = async (req, res, next) => {
     message: "Email registered successfully",
   });
 };
+
+const transporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (req, res, next) => {
+  const { subject, message } = req.body;
+
+  if (!subject || !message) {
+    return next(new expressError(400, "Subject and message are required"));
+  }
+  const emails = await Email.find().select("email -_id");
+  const emailList = emails.map((e) => e.email); 
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: emailList,
+    subject: subject,
+    text: message,
+  };
+
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return next(new expressError(500, "Failed to send emails"));
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Emails sent successfully",
+      });
+    }
+  });
+};
+
 
