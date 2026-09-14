b91891c4b98bfe3298d28b343feb3ba0d014ae86
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-09-14 12:54:47 +0530

    refactor: reorder events to prioritize flagship events at top

--- frontend/src/components/Events/EventsData.js
+++ frontend/src/components/Events/EventsData.js
@@ -1,117 +1,47 @@
 
 const EventsData=[
 
-   {
+  {
+    title:"Bootstrap '26",
+    subheader:"",
+    image:"/Events/bootstrap26.jpeg",
+    typography:"IEEE DTU Orientation Program"
+  },
+  {
     title:"Project WIEVEK",
     subheader:"",
     image:"/Events/wievek.jpeg",
     typography:"Empowering Young Minds in Engineering"
-  },  
-  {
-    title:"Cryptovate",
-    subheader:"",
-    image:"/Events/cryptovate.jpg", 
-    typography:"Online Blockchain Hackathon"
-  },
-   {
-    title:"IEEE Day 2024",
-    subheader:"",
-    image:"/Events/ieeeday24.jpeg",
-    typography:"Celebrating DTU's Oldest Student Society!"
-  },
-   {
-    title:"IEEEXtreme",
-    subheader:"",
-    image:"/Events/ieeextreme.jpeg",
-    typography:"24-hour global programming competition"
-  },
-   {
-    title:"MIST",
-    subheader:"",
-    image:"/Events/mist.jpeg",
-    typography:"The Cryptic Hunt"
-  },
-  
-   {
-    title:"CodeCrunchML",
-    subheader:"",
-    image:"/Events/codecrunch.jpeg",
-    typography:"A Machine Learning Hackathon"
-  },
-   {
-    title:"Case Study",
-    subheader:"",
-    image:"/Events/casestudy.jpeg",
-    typography:"Solutions are born from analysis and daring assumptions"
-  },
- 
-   {
-    title:"TechWeek",
-    subheader:"",
-    image:"/Events/techweek.jpeg",
-    typography:"1 Week IEEE workshops"
   },
   {
-    title:"DSSYWC",
-    subheader:"",
-    image:"/Events/dssywc.jpeg",
-    typography:"DSSYWC 2022"
-  },
-   {
-    title:"Domain Specific Hackathons",
-    subheader:"",
-    image:"/Events/domain.jpeg",
-    typography:"A problem-solving journey!"
-  },
-   {
-    title:"Field Events",
-    subheader:"",
-    image:"/Events/field.jpeg",
-    typography:"Engineering meets the joy of creative competitions!"
-  },
-  
-   {
-    title:"Devcraft",
-    subheader:"",
-    image:"/Events/devcraft.jpeg",
-    typography:"Development-based Hackathon"
-  },
-   {
-    title:"Programming Competitions",
-    subheader:"",
-    image:"/Events/prog.jpeg",
-    typography:"Code weaves the fabric of tomorrow"
-  },
-  
-   {
-    title:"HackAIthon",
-    subheader:"",
-    image:"/Events/hackaithon.jpeg",
-    typography:"Gen AI Hackathon"
-  },
-   {
-    title:"Blockchain",
-    subheader:"",
-    image:"/Events/blockchain.jpeg",
-    typography:"A webinar on Web3 & Blockchain"
-  },
-   {
-    title:"Worldquant",
-    subheader:"",
-    image:"/Events/worldquant.jpeg",
-    typography:"A quantitative finance & fintech challenge"
-  },
-   {
     title:"Vihaan",
     subheader:"",
     image:"/Events/vihaan.jpeg",
-    typography:"India's Largest Student-run Hacakthon"
+    typography:"India's Largest Student-run Hackathon"
   },
-   {
+  {
     title:"Adobe AI Scholastic Program",
     subheader:"",
     image:"/Events/adobeAIscholastic.jpg",
     typography:"Generative AI Workshop by Adobe × IEEE DTU"
+  },
+  {
+    title:"Invictus '26",
+    subheader:"",
+    image:"/Events/invictus.jpeg",
+    typography:"DTU's Annual Technical Fest"
+  },
+  {
+    title:"ILUX",
+    subheader:"",
+    image:"/Events/ilux.jpeg",
+    typography:"Design & UI/UX Competition"
+  },
+  {
+    title:"LFR",
+    subheader:"",
+    image:"/Events/lfr.jpeg",
+    typography:"Line Following Robot Challenge"
   },
   {
     title:"RoboRace",
@@ -124,66 +54,6 @@ const EventsData=[
     subheader:"",
     image:"/Events/micromouse.jpeg",
     typography:"Autonomous Maze-Solving Robot Challenge"
-  },
-  {
-    title:"Bootstrap '26",
-    subheader:"",
-    image:"/Events/bootstrap26.jpeg",
-    typography:"IEEE DTU Orientation Program"
-  },
-  {
-    title:"CodeKaze",
-    subheader:"",
-    image:"/Events/codekaze.jpeg",
-    typography:"Algorithmic Coding Competition"
-  },
-  {
-    title:"Digithon",
-    subheader:"",
-    image:"/Events/digithon.jpeg",
-    typography:"Digital Innovation Hackathon"
-  },
-  {
-    title:"DreamForge",
-    subheader:"",
-    image:"/Events/dreamforge.jpeg",
-    typography:"Business Strategy & Innovation Challenge"
-  },
-  {
-    title:"Guess-a-Palooza",
-    subheader:"",
-    image:"/Events/guess-a-palooza.jpeg",
-    typography:"The Ultimate Guessing Game"
-  },
-  {
-    title:"IEEE Day",
-    subheader:"",
-    image:"/Events/ieee_day.jpeg",
-    typography:"Celebrating IEEE's Global Impact"
-  },
-  {
-    title:"ILUX",
-    subheader:"",
-    image:"/Events/ilux.jpeg",
-    typography:"Design & UI/UX Competition"
-  },
-  {
-    title:"Invictus '26",
-    subheader:"",
-    image:"/Events/invictus.jpeg",
-    typography:"DTU's Annual Technical Fest"
-  },
-  {
-    title:"LFR",
-    subheader:"",
-    image:"/Events/lfr.jpeg",
-    typography:"Line Following Robot Challenge"
-  },
-  {
-    title:"Pitchfork",
-    subheader:"",
-    image:"/Events/pitchfork.jpeg",
-    typography:"Startup Fundraising Simulation"
   },
   {
     title:"Quidditch",
@@ -204,10 +74,136 @@ const EventsData=[
     typography:"Battle of the Bots"
   },
   {
+    title:"CodeCrunchML",
+    subheader:"",
+    image:"/Events/codecrunch.jpeg",
+    typography:"A Machine Learning Hackathon"
+  },
+  {
+    title:"CodeKaze",
+    subheader:"",
+    image:"/Events/codekaze.jpeg",
+    typography:"Algorithmic Coding Competition"
+  },
+  {
+    title:"TechWeek",
+    subheader:"",
+    image:"/Events/techweek.jpeg",
+    typography:"1 Week IEEE workshops"
+  },
+  {
+    title:"IEEEXtreme",
+    subheader:"",
+    image:"/Events/ieeextreme.jpeg",
+    typography:"24-hour global programming competition"
+  },
+  {
+    title:"IEEE Day",
+    subheader:"",
+    image:"/Events/ieee_day.jpeg",
+    typography:"Celebrating IEEE's Global Impact"
+  },
+  {
     title:"TinkerCase",
     subheader:"",
     image:"/Events/tinkercase.jpeg",
     typography:"Arduino & Embedded Systems Showcase"
+  },
+  {
+    title:"DreamForge",
+    subheader:"",
+    image:"/Events/dreamforge.jpeg",
+    typography:"Business Strategy & Innovation Challenge"
+  },
+  {
+    title:"Guess-a-Palooza",
+    subheader:"",
+    image:"/Events/guess-a-palooza.jpeg",
+    typography:"The Ultimate Guessing Game"
+  },
+  {
+    title:"Digithon",
+    subheader:"",
+    image:"/Events/digithon.jpeg",
+    typography:"Digital Innovation Hackathon"
+  },
+  {
+    title:"IEEE Day 2024",
+    subheader:"",
+    image:"/Events/ieeeday24.jpeg",
+    typography:"Celebrating DTU's Oldest Student Society!"
+  },
+  {
+    title:"MIST",
+    subheader:"",
+    image:"/Events/mist.jpeg",
+    typography:"The Cryptic Hunt"
+  },
+  {
+    title:"Case Study",
+    subheader:"",
+    image:"/Events/casestudy.jpeg",
+    typography:"Solutions are born from analysis and daring assumptions"
+  },
+  {
+    title:"DSSYWC",
+    subheader:"",
+    image:"/Events/dssywc.jpeg",
+    typography:"DSSYWC 2022"
+  },
+  {
+    title:"Domain Specific Hackathons",
+    subheader:"",
+    image:"/Events/domain.jpeg",
+    typography:"A problem-solving journey!"
+  },
+  {
+    title:"Field Events",
+    subheader:"",
+    image:"/Events/field.jpeg",
+    typography:"Engineering meets the joy of creative competitions!"
+  },
+  {
+    title:"Devcraft",
+    subheader:"",
+    image:"/Events/devcraft.jpeg",
+    typography:"Development-based Hackathon"
+  },
+  {
+    title:"Programming Competitions",
+    subheader:"",
+    image:"/Events/prog.jpeg",
+    typography:"Code weaves the fabric of tomorrow"
+  },
+  {
+    title:"HackAIthon",
+    subheader:"",
+    image:"/Events/hackaithon.jpeg",
+    typography:"Gen AI Hackathon"
+  },
+  {
+    title:"Blockchain",
+    subheader:"",
+    image:"/Events/blockchain.jpeg",
+    typography:"A webinar on Web3 & Blockchain"
+  },
+  {
+    title:"Worldquant",
+    subheader:"",
+    image:"/Events/worldquant.jpeg",
+    typography:"A quantitative finance & fintech challenge"
+  },
+  {
+    title:"Cryptovate",
+    subheader:"",
+    image:"/Events/cryptovate.jpg", 
+    typography:"Online Blockchain Hackathon"
+  },
+  {
+    title:"Pitchfork",
+    subheader:"",
+    image:"/Events/pitchfork.jpeg",
+    typography:"Startup Fundraising Simulation"
   }
 
 ]
